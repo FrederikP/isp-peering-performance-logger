6de01b6a1ef869b4c7df85c0d187d914891da0c3
Frederik Petersen <frederik@the-imperfection.de>
2019-03-23 22:29:28 +0100

    Switched from tagesschau to ard key for reference

--- measure_download_speed.sh
+++ measure_download_speed.sh
@@ -11,6 +11,6 @@ download_sample () {
 while :
 do
     download_sample 'reddit' 'https://external-preview.redd.it/mp4/Y0CsmhfSFvu_HqysQA1Ed4oD73OtpsYorCIFJrSgvYw-source.mp4?s=c24d071636a35de8bfad866c018bed59672234ae' >> $logfile
-    download_sample 'tagesschau' 'https://dasersteuni-vh.akamaihd.net/i/de/2019/03/22/ee56c487-3f1c-489f-bb13-2c51fd3872ec/,320-1_379735,512-1_379735,960-1_379735,480-1_379735,640-1_379735,1280-1_379735,.mp4.csmil/segment3_5_av.ts?null=0' >> $logfile
+    download_sample 'ard' 'https://dasersteuni-vh.akamaihd.net/i/de/2019/03/22/ee56c487-3f1c-489f-bb13-2c51fd3872ec/,320-1_379735,512-1_379735,960-1_379735,480-1_379735,640-1_379735,1280-1_379735,.mp4.csmil/segment3_5_av.ts?null=0' >> $logfile
     sleep 10m
 done
