6d0b9343b5e4917e9a30b221855d395aeca53125
Frederik Petersen <frederik@the-imperfection.de>
2019-03-28 17:44:38 +0100

    Updated data and renamed tagesschau -> ard to combine old and new reference resource

--- create_charts_bokeh.py
+++ create_charts_bokeh.py
@@ -14,12 +14,14 @@ from bokeh.transform import factor_cmap
 from bokeh.embed import json_item
 
 
-SITES = ['reddit', 'tagesschau']
+SITES = ['reddit', 'ard']
 
 def create_chart(server):
 
     with open(f'{server}.log') as csv_file:
         speed_data = pd.read_csv(csv_file, sep=' ', header=None, names=['site', 'timestamp', 'speed'], parse_dates=['timestamp'])
+    
+    speed_data.loc[speed_data.site == 'tagesschau', 'site'] = 'ard'
 
     timeindexed = speed_data.set_index(pd.DatetimeIndex(speed_data['timestamp']))
     grouped = timeindexed.groupby(['site', pd.Grouper(freq='2h')])
@@ -28,13 +30,13 @@ def create_chart(server):
     # Offset to actually have the timestamp in the middle of the 2h timewindow
     grouped['timestamp'] = grouped['timestamp'] + pd.DateOffset(hours=1)
     reddit_grouped = grouped[grouped['site'] == 'reddit']
-    tagesschau_grouped = grouped[grouped['site'] == 'tagesschau']
+    ard_grouped = grouped[grouped['site'] == 'ard']
 
     output_file(f"{server}.html")
 
     raw_data_source = ColumnDataSource(speed_data)
     avg_data_source_reddit = ColumnDataSource(reddit_grouped)
-    avg_data_source_tagesschau = ColumnDataSource(tagesschau_grouped)
+    avg_data_source_ard = ColumnDataSource(ard_grouped)
 
     now = datetime.utcnow().replace(tzinfo=pytz.utc)
     week_ago = now - timedelta(days=7)
@@ -54,7 +56,7 @@ def create_chart(server):
 
     scatter = chart.scatter(x='timestamp', y='speed', source=raw_data_source, color=factor_cmap('site', ['red', 'blue'], SITES), legend="site", fill_alpha=0.1, line_alpha=0)
     line_reddit = chart.line(x='timestamp_', y='speed_mean', source=avg_data_source_reddit, line_color='red', legend="reddit average", line_dash='dotted', line_width=3)
-    line_tagesschau = chart.line(x='timestamp_', y='speed_mean', source=avg_data_source_tagesschau, line_color='blue', legend="tagesschau average", line_dash='dashed', line_width=3)
+    line_ard = chart.line(x='timestamp_', y='speed_mean', source=avg_data_source_ard, line_color='blue', legend="ard average", line_dash='dashed', line_width=3)
 
     chart.add_tools(HoverTool(
         tooltips=[
@@ -78,7 +80,7 @@ def create_chart(server):
         formatters={
             'timestamp_': 'datetime'
         },
-        renderers=[line_reddit, line_tagesschau],
+        renderers=[line_reddit, line_ard],
         toggleable= False
     ))
 
