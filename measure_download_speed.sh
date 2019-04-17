bfc18f8d907ce7e422fa935cbdb11bdcc7691b9a
Frederik Petersen <frederik@the-imperfection.de>
2019-04-17 09:16:32 +0200

    Changed ard url to one that should stay up for a year

--- measure_download_speed.sh
+++ measure_download_speed.sh
@@ -11,6 +11,6 @@ download_sample () {
 while :
 do
     download_sample 'reddit' 'https://external-preview.redd.it/mp4/Y0CsmhfSFvu_HqysQA1Ed4oD73OtpsYorCIFJrSgvYw-source.mp4?s=c24d071636a35de8bfad866c018bed59672234ae' >> $logfile
-    download_sample 'ard' 'https://dasersteuni-vh.akamaihd.net/i/de/2019/03/22/ee56c487-3f1c-489f-bb13-2c51fd3872ec/,320-1_379735,512-1_379735,960-1_379735,480-1_379735,640-1_379735,1280-1_379735,.mp4.csmil/segment3_5_av.ts?null=0' >> $logfile
+    download_sample 'ard' 'https://dasersteuni-vh.akamaihd.net/i/int/2019/04/15/3f90bbab-6d00-4a57-8d6f-167fff3f32e1/,960-1_391599,640-1_391599,512-1_391599,480-1_391599,320-1_391599,1280-1_391599,.mp4.csmil/segment13_5_av.ts?null=0' >> $logfile
     sleep 10m
 done
